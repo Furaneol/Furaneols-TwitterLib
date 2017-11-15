625428f0bc8a74f40c39cda25b982f64860efc77
Furaneol <km_katsu00@hotmail.com>
2017-11-15 23:26:39 +0900

    UrlEncodeメソッドを前プロジェクトからサルベージ

--- TwitterLib/Twitter.cs
+++ TwitterLib/Twitter.cs
@@ -8,5 +8,36 @@ namespace TwitterLib
 {
     public class Twitter
     {
+        const string acceptString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~";
+        /// <summary>
+        /// Twitter用のURLエンコードメソッドです。
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private static string UrlEncode(string value)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < value.Length; i++)
+            {
+                bool isSurrogate = i < value.Length - 1 && Char.IsSurrogatePair(value[i], value[i + 1]);
+                string s = value.Substring(i, isSurrogate ? 2 : 1);
+                if (s.Length == 1 && acceptString.IndexOf(s) >= 0)
+                {
+                    builder.Append(s);
+                }
+                else
+                {
+                    byte[] buffer = Encoding.UTF8.GetBytes(s);
+                    foreach (byte data in buffer)
+                    {
+                        builder.Append("%" + data.ToString("X2"));
+                    }
+                }
+                if (isSurrogate)
+                    i++;
+            }
+            return builder.ToString();
+        }
+
     }
 }
