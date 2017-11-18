17d500cf86214f4b8a5fae017fc342aca6250050
Furaneol <km_katsu00@hotmail.com>
2017-11-18 15:04:12 +0900

    <summary>追加

--- TwitterLib/Entities/Symbol.cs
+++ TwitterLib/Entities/Symbol.cs
@@ -7,11 +7,20 @@ using System.Runtime.Serialization;
 
 namespace TwitterLib.Entities
 {
+    /// <summary>
+    /// ティッカーシンボルの情報を格納するオブジェクトです。
+    /// </summary>
     [DataContract]
     public class Symbol
     {
+        /// <summary>
+        /// ツイート内におけるティッカーシンボルの範囲を取得します。
+        /// </summary>
         [DataMember(Name = "indices")]
         public int[] Indices { get; internal set; }
+        /// <summary>
+        /// ティッカーシンボルの名前を取得します。
+        /// </summary>
         [DataMember(Name = "text")]
         public string Text { get; internal set; }
     }
