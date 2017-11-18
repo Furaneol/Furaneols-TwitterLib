904764287efd95d2b2bde42d812961ebb42cb1d8
Furaneol <km_katsu00@hotmail.com>
2017-11-18 13:05:34 +0900

    <summary>更新

--- TwitterLib/Entities/Url.cs
+++ TwitterLib/Entities/Url.cs
@@ -7,26 +7,29 @@ using System.Runtime.Serialization;
 
 namespace TwitterLib.Entities
 {
+    /// <summary>
+    /// ツイートに埋め込まれたURLの情報を格納するオブジェクトです。
+    /// </summary>
     [DataContract]
     public class Url
     {
         /// <summary>
-        /// 
+        /// ツイート投稿時に入力されたURLを取得します。
         /// </summary>
         [DataMember(Name = "display_url")]
         public string DisplayUrl { get; internal set; }
         /// <summary>
-        /// 
+        /// ツイート登校時に入力されたURLをプロトコル付きで取得します。
         /// </summary>
         [DataMember(Name = "expanded_url")]
         public string ExpandedUrl { get; internal set; }
         /// <summary>
-        /// 
+        /// ツイート内におけるURLの範囲を取得します。
         /// </summary>
         [DataMember(Name = "indices")]
         public int[] Indices { get; internal set; }
         /// <summary>
-        /// 
+        /// ツイートに埋め込まれた短縮URLを取得します。
         /// </summary>
         [DataMember(Name = "url")]
         public string UrlString { get; internal set; }
