2731be5456cd08105710d5be0683ada79f03c631
Furaneol <km_katsu00@hotmail.com>
2017-11-18 17:49:25 +0900

    usingにSystem.Runtime.Serialization.JsonおよびSystem.Netを追加。使用しない予定のusingを削除

--- TwitterLib/Twitter.cs
+++ TwitterLib/Twitter.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.Net;
 
 namespace TwitterLib
 {
