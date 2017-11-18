b6edd8e34f3a53c5781628145f5a44ae846fc2d7
Furaneol <km_katsu00@hotmail.com>
2017-11-18 12:51:21 +0900

    <summary>更新

--- TwitterLib/Entities/Mention.cs
+++ TwitterLib/Entities/Mention.cs
@@ -14,27 +14,27 @@ namespace TwitterLib.Entities
     public class Mention
     {
         /// <summary>
-        /// 
+        /// 言及先のユーザーIDを取得します。
         /// </summary>
         [DataMember(Name = "id")]
         public ulong ID { get; internal set; }
         /// <summary>
-        /// 
+        /// 言及先のユーザーIDを文字列として取得します。
         /// </summary>
         [DataMember(Name = "id_str")]
         public string StringID { get; internal set; }
         /// <summary>
-        /// 
+        /// 言及先ユーザー名の範囲を取得します。
         /// </summary>
         [DataMember(Name = "indices")]
         public int[] Indices { get; internal set; }
         /// <summary>
-        /// 
+        /// 言及先の表示名を取得します。
         /// </summary>
         [DataMember(Name = "name")]
         public string Name { get; internal set; }
         /// <summary>
-        /// 
+        /// 言及先のスクリーン名を取得します。
         /// </summary>
         [DataMember(Name = "screen_name")]
         public string ScreenName { get; internal set; }
