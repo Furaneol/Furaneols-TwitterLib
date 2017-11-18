1e48dd28a6f9f487c32646c86fce1dbfae2e2e6b
Furaneol <km_katsu00@hotmail.com>
2017-11-18 12:46:05 +0900

    Coordinatesクラスを定義

---
+++ TwitterLib/Coordinates.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace TwitterLib
+{
+    [DataContract]
+    class InterCoordinates
+    {
+        [DataMember(Name ="coodinates")]
+        public float[] Coordinates { get; internal set; }
+    }
+}
