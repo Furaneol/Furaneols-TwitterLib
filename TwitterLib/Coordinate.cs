2a2e4d3e3e18ca176a01b7154689a51df9a76092
Furaneol <km_katsu00@hotmail.com>
2017-11-18 15:51:02 +0900

    Coordinateクラス追加。Tweet.Coordinateプロパティ作成

---
+++ TwitterLib/Coordinate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace TwitterLib
+{
+    /// <summary>
+    /// 位置情報を格納するオブジェクトです。
+    /// </summary>
+    [DataContract]
+    public class Coordinate
+    {
+        /// <summary>
+        /// 座標情報を取得します。
+        /// </summary>
+        [DataMember(Name ="coodinates")]
+        public float[] Coodinates { get; internal set; }
+        /// <summary>
+        /// この情報の種類を示す文字列を取得します。
+        /// </summary>
+        [DataMember(Name = "type")]
+        public string Type { get; internal set; }
+    }
+}
--- TwitterLib/Tweet.cs
+++ TwitterLib/Tweet.cs
@@ -22,49 +22,54 @@ namespace TwitterLib
         /// </summary>
         public DateTime CreatedAt { get; internal set; }
         /// <summary>
-        /// 
+        /// ツイートの識別番号を取得します。
         /// </summary>
         [DataMember(Name = "id")]
         public ulong ID { get; internal set; }
         /// <summary>
-        /// 
+        /// ツイートの識別番号を文字列として取得します。
         /// </summary>
         [DataMember(Name ="id_str")]
         public string StringID { get; internal set; }
         /// <summary>
-        /// 
+        /// ツイートを投稿したアプリケーションの情報を示す文字列を取得します。
         /// </summary>
         [DataMember(Name = "source")]
         public string Source { get; internal set; }
         /// <summary>
-        /// 
+        /// ツイートの切り捨てが発生しているかどうかを取得します。
         /// </summary>
         [DataMember(Name = "truncated")]
         public bool Truncated { get; internal set; }
         /// <summary>
-        /// 
+        /// 返信先のツイートIDを取得します。このツイートが返信でない場合はnullを返します。
         /// </summary>
         [DataMember(Name = "in_reply_to_status_id")]
         public ulong? InReplyToStatusID { get; internal set; }
         /// <summary>
-        /// 
+        /// 返信先のツイートIDを文字列として取得します。このツイートが返信でない場合はnullを返します。
         /// </summary>
         [DataMember(Name ="in_reply_to_status_id_str")]
         public string InReplyToStatusStringID { get; internal set; }
         /// <summary>
-        /// 
+        /// 返信先のユーザーIDを取得します。このツイートが返信でない場合はnullを返します。
         /// </summary>
         [DataMember(Name ="in_reply_to_user_id")]
         public ulong? InReplyToUserID { get; internal set; }
         /// <summary>
-        /// 
+        /// 返信先のユーザーIDを文字列として取得します。このツイートが返信でない場合はnullを返します。
         /// </summary>
         [DataMember(Name = "in_reply_to_user_id_str")]
         public string InReplyToUserStringID { get; internal set; }
         /// <summary>
-        /// 
+        /// 投稿したユーザーの情報を取得します。
         /// </summary>
         [DataMember(Name ="user")]
         public User User { get; internal set; }
+        /// <summary>
+        /// 投稿した位置の情報を取得します。
+        /// </summary>
+        [DataMember(Name = "coordinates")]
+        public Coordinate Coordinate { get; internal set; }
     }
 }
