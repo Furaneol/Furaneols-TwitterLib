d80424d0850138e0c179b1f5ed305841f101ef62
Furaneol <km_katsu00@hotmail.com>
2017-11-27 16:03:46 +0900

    GetHomeTimelineメソッド実装

--- TwitterLib/HomeTimeline.cs
+++ TwitterLib/HomeTimeline.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
+using System.Runtime.Serialization.Json;
+using System.IO;
 
 namespace TwitterLib
 {
     public partial class Twitter
     {
-        public static Tweet[] GetHomeTimeline(int? count = null, ulong? sinceId = null, ulong? maxId = null, bool? trimUser = null, bool? excludeReplies = null, bool? includeEntities = null)
+        public Tweet[] GetHomeTimeline(int? count = null, ulong? sinceId = null, ulong? maxId = null, bool? trimUser = null, bool? excludeReplies = null, bool? includeEntities = null)
         {
             SortedDictionary<string, string> args = new SortedDictionary<string, string>();
             if (count.HasValue)
@@ -24,7 +26,10 @@ namespace TwitterLib
                 args["exclude_replies"] = excludeReplies.ToString().ToLower();
             if (includeEntities.HasValue)
                 args["include_entities"] = includeEntities.ToString().ToLower();
-            
+            Tweet[] timeline = null;
+            DataContractJsonSerializer js = new DataContractJsonSerializer(typeof(Tweet[]));
+            GetOAuthResponce("GET", "", args, (Stream stream) => { timeline = (Tweet[])js.ReadObject(stream); });
+            return timeline;
         }
     }
 }
