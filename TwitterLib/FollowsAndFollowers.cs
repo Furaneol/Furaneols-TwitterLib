45edf0aa2bd9b29c98231ded55e58ee680d182d8
Furaneol <km_katsu00@hotmail.com>
2017-12-01 14:56:38 +0900

    Twitter.GetFollowerIdListおよびTwitter.GetFollowerListの実装

--- TwitterLib/FollowsAndFollowers.cs
+++ TwitterLib/FollowsAndFollowers.cs
@@ -18,18 +18,7 @@ namespace TwitterLib
         /// <returns></returns>
         public IEnumerator<ulong> GetFollowerIdList(ulong? cursor = null, int? count = null)
         {
-            SortedDictionary<string, string> args = new SortedDictionary<string, string> { ["user_id"] = UserID.ToString() };
-            if (count.HasValue)
-                args["count"] = count.ToString();
-            do
-            {
-                if (cursor.HasValue)
-                    args["cursor"] = cursor.ToString();
-                IdContainer container = (IdContainer)Parent.GetOAuthResponce("GET", "https://api.twitter.com/1.1/followers/ids.json", args, typeof(IdContainer));
-                cursor = container.NextCursor;
-                foreach (ulong id in container.IDList)
-                    yield return id;
-            } while (cursor != 0);
+            return Parent.GetFollowerIdList(UserID, cursor, count);
         }
         /// <summary>
         /// 現在のユーザーをフォローしている人(フォロワー)の一覧を取得します。
@@ -41,22 +30,7 @@ namespace TwitterLib
         /// <returns></returns>
         public IEnumerator<User> GetFollowerList(ulong? cursor = null, int? count = null, bool? skipStatus = null, bool? includeUserEntities = null)
         {
-            SortedDictionary<string, string> args = new SortedDictionary<string, string>() { ["user_id"] = UserID.ToString() };
-            if (count.HasValue)
-                args["count"] = count.ToString();
-            if (skipStatus.HasValue)
-                args["skip_status"] = skipStatus.ToString().ToLower();
-            if (includeUserEntities.HasValue)
-                args["include_user_entities"] = includeUserEntities.ToString().ToLower();
-            do
-            {
-                if (cursor.HasValue)
-                    args["cursor"] = cursor.ToString();
-                UserContainer container = (UserContainer)Parent.GetOAuthResponce("GET", "https://api.twitter.com/1.1/followers/list.json", args, typeof(UserContainer));
-                cursor = container.NextCursor;
-                foreach(User user in container.Users)
-                    yield return user;
-            } while (cursor != 0);
+            return Parent.GetFollowerList(UserID, cursor, count, skipStatus, includeUserEntities);
         }
         /// <summary>
         /// 現在のユーザーがフォローしている人のID一覧を取得します。
@@ -66,18 +40,6 @@ namespace TwitterLib
         /// <returns></returns>
         public IEnumerator<ulong> GetFollowIdList(ulong? cursor = null, int? count = null)
         {
-            SortedDictionary<string, string> args = new SortedDictionary<string, string>() { ["user_id"] = UserID.ToString() };
-            if (count.HasValue)
-                args["count"] = count.ToString();
-            do
-            {
-                if (cursor.HasValue)
-                    args["cursor"] = cursor.ToString();
-                IdContainer container = (IdContainer)Parent.GetOAuthResponce("GET", "https://api.twitter.com/1.1/friends/ids.json", args, typeof(IdContainer));
-                cursor = container.NextCursor;
-                foreach (ulong id in container.IDList)
-                    yield return id;
-            } while (cursor != 0);
         }
         /// <summary>
         /// 現在のユーザーがフォローしている人の一覧を取得します。
@@ -89,27 +51,12 @@ namespace TwitterLib
         /// <returns></returns>
         public IEnumerator<User> GetFollowList(ulong? cursor = null, int? count = null, bool? skipStatus = null, bool? includeUserEntities = null)
         {
-            SortedDictionary<string, string> args = new SortedDictionary<string, string>() { ["user_id"] = UserID.ToString() };
-            if (count.HasValue)
-                args["count"] = count.ToString();
-            if (skipStatus.HasValue)
-                args["skip_status"] = skipStatus.ToString().ToLower();
-            if (includeUserEntities.HasValue)
-                args["include_user_entities"] = includeUserEntities.ToString().ToLower();
-            do
-            {
-                if (cursor.HasValue)
-                    args["cursor"] = cursor.ToString();
-                UserContainer container = (UserContainer)Parent.GetOAuthResponce("GET", "https://api.twitter.com/1.1/friends/list.json", args, typeof(UserContainer));
-                cursor = container.NextCursor;
-                foreach (User user in container.Users)
-                    yield return user;
-            } while (cursor != 0);
         }
     }
 
     public partial class Twitter
     {
+        #region GetFollowerIdList
         /// <summary>
         /// 指定されたIDを持つユーザーのフォロワーのID一覧を取得します。
         /// </summary>
@@ -148,6 +95,56 @@ namespace TwitterLib
                     yield return id;
             } while (cursor != 0);
         }
+        #endregion
 
+        #region GetFollowerList
+        /// <summary>
+        /// 指定されたIDを持つユーザーのフォロワー一覧を取得します。
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="cursor"></param>
+        /// <param name="count"></param>
+        /// <param name="skipStatus"></param>
+        /// <param name="includeUserEntities"></param>
+        /// <returns></returns>
+        public IEnumerator<User> GetFollowerList(ulong id, ulong? cursor = null, int? count = null, bool? skipStatus = null, bool? includeUserEntities = null)
+        {
+            SortedDictionary<string, string> args = new SortedDictionary<string, string>() { ["user_id"] = id.ToString() };
+            return getFollowerList(args, cursor, count, skipStatus, includeUserEntities);
+        }
+        /// <summary>
+        /// 指定されたスクリーン名を持つユーザーのフォロワー一覧を取得します。
+        /// </summary>
+        /// <param name="screenName"></param>
+        /// <param name="cursor"></param>
+        /// <param name="count"></param>
+        /// <param name="skipStatus"></param>
+        /// <param name="includeUserEntities"></param>
+        /// <returns></returns>
+        public IEnumerator<User> GetFollowerList(string screenName, ulong? cursor = null, int? count = null, bool? skipStatus = null, bool? includeUserEntities = null)
+        {
+            SortedDictionary<string, string> args = new SortedDictionary<string, string>() { ["screen_name"] = screenName };
+            return getFollowerList(args, cursor, count, skipStatus, includeUserEntities);
+        }
+
+        private IEnumerator<User> getFollowerList(SortedDictionary<string, string> args, ulong? cursor, int? count, bool? skipStatus, bool? includeUserEntities)
+        {
+            if (count.HasValue)
+                args["count"] = count.ToString();
+            if (skipStatus.HasValue)
+                args["skip_status"] = skipStatus.ToString().ToLower();
+            if (includeUserEntities.HasValue)
+                args["include_user_entities"] = includeUserEntities.ToString().ToLower();
+            do
+            {
+                if (cursor.HasValue)
+                    args["cursor"] = cursor.ToString();
+                UserContainer container = (UserContainer)GetOAuthResponce("GET", "https://api.twitter.com/1.1/followers/list.json", args, typeof(UserContainer));
+                cursor = container.NextCursor;
+                foreach (User user in container.Users)
+                    yield return user;
+            } while (cursor != 0);
+        }
+        #endregion
     }
 }
