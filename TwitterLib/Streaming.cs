8ce041072415886ddc148c61f84d2d852ef2aded
Furaneol <km_katsu00@hotmail.com>
2017-12-07 20:12:05 +0900

    EventInfoクラスを更新(基本クラス化) TweetEventInfoクラスを追加

--- TwitterLib/Streaming.cs
+++ TwitterLib/Streaming.cs
@@ -268,16 +268,45 @@ namespace TwitterLib
         public string ResponceContent { get; }
     }
     /// <summary>
-    /// イベント情報オブジェクトです。
+    /// イベントの情報オブジェクトの基本クラスです。
     /// </summary>
+    [DataContract]
     public class EventInfo : StreamingMessage
     {
-        public override StreamingMessageType MessageType => throw new NotImplementedException();
-
-        public override Twitter Parent { get => throw new NotImplementedException(); internal set => throw new NotImplementedException(); }
+        public override StreamingMessageType MessageType => StreamingMessageType.Events;
+        public override Twitter Parent { get; internal set; }
+        /// <summary>
+        /// イベントの名前を取得します。
+        /// </summary>
+        [DataMember(Name = "event")]
+        public string EventName { get; private set; }
+        /// <summary>
+        /// イベントの発生日時を取得します。
+        /// </summary>
+        [DataMember(Name = "created_at")]
+        public DateTime CreatedAt { get; private set; }
+        /// <summary>
+        /// イベントの対象のユーザーを取得します。
+        /// </summary>
+        [DataMember(Name = "target")]
+        public User TargetUser { get; private set; }
+        /// <summary>
+        /// イベントを発生させたユーザーを取得します。
+        /// </summary>
+        [DataMember(Name = "source")]
+        public User SourceUser { get; private set; }
+    }
+    [DataContract]
+    public class TweetEventInfo : EventInfo
+    {
+        /// <summary>
+        /// 対象のツイートを取得します。
+        /// </summary>
+        [DataMember(Name ="target_object")]
+        public Tweet TargetedTweet { get; private set; }
     }
     /// <summary>
-    /// ストリーミングメッセージに対する汎用デシリアライズクラスです。
+    /// ストリーミングメッセージの内容を判別する為の汎用デシリアライザです。
     /// </summary>
     [DataContract]
     internal class StreamingContract
@@ -302,5 +331,8 @@ namespace TwitterLib
 
         [DataMember(Name ="warning")]
         public WarningInfo warning { get; private set; }
+
+        [DataMember(Name = "events")]
+        public string EventName { get; private set; }
     }
 }
