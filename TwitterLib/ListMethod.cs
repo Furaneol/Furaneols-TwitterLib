9811422c0276dcf91125b83981df12473278aa3d
Furaneol <km_katsu00@hotmail.com>
2017-12-19 12:02:37 +0900

    ListMethodファイルを追加

---
+++ TwitterLib/ListMethod.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace TwitterLib
+{
+    public partial class List
+    {
+        /// <summary>
+        /// リストに登録されているユーザーの一覧を取得します。
+        /// </summary>
+        /// <param name="id">リストのID</param>
+        /// <param name="count">1ページあたりのユーザー数</param>
+        /// <param name="includeEntities">Entitiesノードを含むかどうか</param>
+        /// <param name="skipStatus">statusノードを省略するかどうか</param>
+        /// <param name="cursor">カーソル</param>
+        /// <returns></returns>
+        public IEnumerable<User> GetMemberList(ulong id, int? count = null, bool? includeEntities = null, bool? skipStatus = null, ulong? cursor = null)
+        {
+            SortedDictionary<string, string> args = new SortedDictionary<string, string>() { ["list_id"] = id.ToString() };
+            if (count.HasValue)
+                args["count"] = count.ToString();
+            if (includeEntities.HasValue)
+                args["include_entities"] = includeEntities.ToString().ToLower();
+            if (skipStatus.HasValue)
+                args["skip_status"] = skipStatus.ToString().ToLower();
+            do
+            {
+                if (cursor.HasValue)
+                    args["cursor"] = cursor.ToString();
+                UserContainer container = (UserContainer)Parent.GetOAuthResponce("GET", "https://api.twitter.com/1.1/lists/members.json", args, typeof(UserContainer));
+                cursor = container.NextCursor;
+                foreach (User user in container.Users)
+                    yield return user;
+            } while (cursor != 0);
+        }
+
+
+    }
+}
