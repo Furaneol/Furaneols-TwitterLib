8759fac1cc75eb5fbc7bf5792670cb724a0b5ad8
Furaneol <km_katsu00@hotmail.com>
2018-01-15 15:30:45 +0900

    Twitter.FavoriteメソッドおよびTwitter.Unfavoriteメソッド実装

--- TwitterLib/Favorite.cs
+++ TwitterLib/Favorite.cs
@@ -8,6 +8,43 @@ namespace TwitterLib
 {
     partial class Twitter
     {
+        /// <summary>
+        /// 指定されたツイートにいいねします。
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Tweet Favorite(ulong id)
+        {
+            SortedDictionary<string, string> args = new SortedDictionary<string, string>() { ["id"] = id.ToString() };
+            return (Tweet)GetOAuthResponce("POST", "https://api.twitter.com/1.1/favorites/create.json", args, typeof(Tweet));
+        }
+        /// <summary>
+        /// 指定されたツイートのいいねを解除します。
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Tweet Unfavorite(ulong id)
+        {
+            SortedDictionary<string, string> args = new SortedDictionary<string, string>() { ["id"] = id.ToString() };
+            return (Tweet)GetOAuthResponce("POST", "https://api.twitter.com/1.1/favorites/destroy.json", args, typeof(Tweet));
+        }
+    }
 
+    partial class Tweet
+    {
+        /// <summary>
+        /// ツイートをいいねします。
+        /// </summary>
+        public void Favorite()
+        {
+            Parent?.Favorite(ID);
+        }
+        /// <summary>
+        /// ツイートのいいねを解除します。
+        /// </summary>
+        public void Unfavorite()
+        {
+            Parent?.Unfavorite(ID);
+        }
     }
 }
