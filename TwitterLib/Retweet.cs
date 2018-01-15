cfc16268d0a8d2f81f6c483a2f8bb254e3202921
Furaneol <km_katsu00@hotmail.com>
2018-01-15 17:15:21 +0900

    Twitter.RetweetメソッドおよびTwitter.Unretweetメソッド実装

--- TwitterLib/Retweet.cs
+++ TwitterLib/Retweet.cs
@@ -8,11 +8,43 @@ namespace TwitterLib
 {
     partial class Twitter
     {
-
+        /// <summary>
+        /// 指定されたIDのツイートをリツイートします。
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Tweet Retweet(ulong id)
+        {
+            return (Tweet)GetOAuthResponce("POST", "https://api.twitter.com/1.1/statuses/retweet/" + id + ".json", new SortedDictionary<string, string>() { ["id"] = id.ToString() }, typeof(Tweet));
+        }
+        /// <summary>
+        /// 指定されたIDのリツイートを解除します。
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Tweet Unretweet(ulong id)
+        {
+            return (Tweet)GetOAuthResponce("POST", "https://api.twitter.com/1.1/statuses/unretweet/" + id + ".json", new SortedDictionary<string, string>() { ["id"] = id.ToString() }, typeof(Tweet));
+        }
     }
 
     partial class Tweet
     {
-
+        /// <summary>
+        /// 現在のツイートをリツイートします。
+        /// </summary>
+        /// <seealso cref="Twitter.Retweet(ulong)"/>
+        public void Retweet()
+        {
+            Parent?.Retweet(ID);
+        }
+        /// <summary>
+        /// 現在のツイートのリツイートを解除します。
+        /// </summary>
+        /// <seealso cref="Twitter.Unretweet(ulong)"/>
+        public void UnRetweet()
+        {
+            Parent?.Unretweet(ID);
+        }
     }
 }
