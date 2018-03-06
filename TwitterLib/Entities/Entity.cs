5d957d344274d16049bdd13c3af8855339d5f3f3
Furaneol <km_katsu00@hotmail.com>
2018-03-06 14:31:42 +0900

    Entityの各配列に設定された値が無い場合、nullではなく空配列を返すように修正

--- TwitterLib/Entities/Entity.cs
+++ TwitterLib/Entities/Entity.cs
@@ -13,30 +13,56 @@ namespace TwitterLib.Entities
     [DataContract]
     public class Entity
     {
+
+        HashTag[] hashTags;
+        Media[] media;
+        Url[] urls;
+        Mention[] mentions;
+        Symbol[] symbols;
         /// <summary>
         /// ツイートに含まれているハッシュタグの一覧を取得します。
         /// </summary>
-        [DataMember(Name ="hashtags")]
-        public HashTag[] HashTags { get; internal set; }
+        [DataMember(Name = "hashtags")]
+        public HashTag[] HashTags
+        {
+            get => hashTags;
+            internal set { hashTags = value ?? new HashTag[] { }; }
+        }
         /// <summary>
         /// ツイートに埋め込まれているメディアの一覧を取得します。
         /// </summary>
         [DataMember(IsRequired = false, Name = "media")]
-        public Media[] MediaList { get; internal set; }
+        public Media[] MediaList
+        {
+            get => media;
+            internal set { media = value ?? new Media[] { }; }
+        }
         /// <summary>
         /// ツイートに含まれているURLの一覧を取得します。
         /// </summary>
         [DataMember(Name = "urls")]
-        public Url[] Urls { get; internal set; }
+        public Url[] Urls
+        {
+            get => urls;
+            internal set { urls = value ?? new Url[] { }; }
+        }
         /// <summary>
         /// ツイートに含まれている言及先の一覧を取得します。
         /// </summary>
         [DataMember(Name = "mentions")]
-        public Mention[] Mentions { get; internal set; }
+        public Mention[] Mentions
+        {
+            get => mentions;
+            internal set { mentions = value ?? new Mention[] { }; }
+        }
         /// <summary>
         /// ツイートに含まれているシンボルの一覧を取得します。
         /// </summary>
-        [DataMember(Name ="symbols")]
-        public Symbol[] Symbols { get; internal set; }
+        [DataMember(Name = "symbols")]
+        public Symbol[] Symbols
+        {
+            get => symbols;
+            internal set { symbols = value ?? new Symbol[] { }; }
+        }
     }
 }
