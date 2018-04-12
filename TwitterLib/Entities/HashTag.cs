09ce8693cf7d6344b579e6dd86d3de866ebfa77c
Furaneol <km_katsu00@hotmail.com>
2018-04-12 12:23:00 +0900

    <summary>の内容を追加

--- TwitterLib/Entities/HashTag.cs
+++ TwitterLib/Entities/HashTag.cs
@@ -8,18 +8,18 @@ using System.Runtime.Serialization;
 namespace TwitterLib.Entities
 {
     /// <summary>
-    /// 
+    /// ハッシュタグ情報を格納するオブジェクトです。
     /// </summary>
     [DataContract]
     public class HashTag
     {
         /// <summary>
-        /// 
+        /// ハッシュタグの範囲を取得します。
         /// </summary>
         [DataMember(Name = "indices")]
         public int[] Indices { get; internal set; }
         /// <summary>
-        /// 
+        /// ハッシュタグ文字列を取得します。
         /// </summary>
         [DataMember(Name = "text")]
         public string Text { get; internal set; }
