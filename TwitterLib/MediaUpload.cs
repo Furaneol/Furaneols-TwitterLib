a40ecf1814162c547e64a0d421e84e052b0ca7af
Furaneol <km_katsu00@hotmail.com>
2018-10-23 23:15:39 +0900

    MediaUpload.csを追加

---
+++ TwitterLib/MediaUpload.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+
+namespace TwitterLib
+{
+    partial class Twitter
+    {
+        /// <summary>
+        /// 指定された画像をアップロードします。
+        /// </summary>
+        /// <param name="image"></param>
+        public void UploadMedia(Image image)
+        {
+
+        }
+
+        public void UploadMedia(string mediaType, Stream stream)
+        {
+
+        }
+    }
+}
