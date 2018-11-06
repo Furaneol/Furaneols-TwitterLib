3aedc29092cbe35d4fd6eda96ba66fe4cfe4ae9c
Furaneol <km_katsu00@hotmail.com>
2018-11-06 21:12:49 +0900

    Twitter.GetAvailableTrendPlacesメソッドを実装

--- TwitterLib/Trend.cs
+++ TwitterLib/Trend.cs
@@ -1,13 +1,17 @@
-﻿using System;
+﻿using TwitterLib.Trends;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TwitterLib
 {
     public partial class Twitter
     {
-
+        /// <summary>
+        /// トレンド取得時に使用可能な位置情報の一覧を取得します。
+        /// </summary>
+        /// <returns></returns>
+        public TrendPlaceInfo[] GetAvailableTrendPlaces()
+        {
+            return (TrendPlaceInfo[])GetOAuthResponce(TwitterAuthenticationMode.UserAuthentication, "GET", "https://api.twitter.com/1.1/trends/available.json", new SortedDictionary<string, string>(), typeof(TrendPlaceInfo[]));
+        }
     }
 }
