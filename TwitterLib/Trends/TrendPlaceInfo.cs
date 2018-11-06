f7a8d2b86d1139b71edbd5babf95856128b86c7e
Furaneol <km_katsu00@hotmail.com>
2018-11-06 20:28:43 +0900

    TrendPlaceInfoクラスおよびPlaceTypeクラスを実装

--- TwitterLib/Trends/TrendPlaceInfo.cs
+++ TwitterLib/Trends/TrendPlaceInfo.cs
@@ -8,11 +8,62 @@ using System.Runtime.Serialization;
 namespace TwitterLib.Trends
 {
     /// <summary>
-    /// 
+    /// トレンド取得時に指定できる位置情報を示すデータを格納するオブジェクトです。
     /// </summary>
     [DataContract]
     public class TrendPlaceInfo
     {
-
+        /// <summary>
+        /// 位置情報の名前を取得します。
+        /// </summary>
+        [DataMember(Name = "name")]
+        public string Name { get; private set; }
+        /// <summary>
+        /// 位置情報の種類を取得します。
+        /// </summary>
+        [DataMember(Name = "placeType")]
+        public PlaceType Type { get; private set; }
+        /// <summary>
+        /// 位置情報に紐づいたwhere.yahooapis.comのURLを取得します。
+        /// </summary>
+        [DataMember(Name = "url")]
+        public string Url { get; private set; }
+        /// <summary>
+        /// 現在の位置情報を含む親情報のWOEIDを取得します。
+        /// </summary>
+        [DataMember(Name = "parentid")]
+        public int ParentID { get; private set; }
+        /// <summary>
+        /// 国名を取得します。
+        /// </summary>
+        [DataMember(Name = "country")]
+        public string Country { get; private set; }
+        /// <summary>
+        /// 位置情報に割り当てられたYahoo! Where on Earth IDを取得します。
+        /// </summary>
+        [DataMember(Name = "woeid")]
+        public int WOEID { get; private set; }
+        /// <summary>
+        /// 国名コードを取得します。
+        /// </summary>
+        [DataMember(Name = "countryCode")]
+        public string CountryCode { get; private set; }
+    }
+    /// <summary>
+    /// 位置情報の種類を示すデータを格納するオブジェクトです。
+    /// </summary>
+    [DataContract]
+    public class PlaceType
+    {
+        /// <summary>
+        /// 位置情報種別コードを取得します。
+        /// </summary>
+        [DataMember(Name = "code")]
+        public int Code { get; private set; }
+        /// <summary>
+        /// 位置情報種別の名前を取得します。
+        /// </summary>
+        [DataMember(Name = "name")]
+        public string Name { get; private set; }
     }
 }
