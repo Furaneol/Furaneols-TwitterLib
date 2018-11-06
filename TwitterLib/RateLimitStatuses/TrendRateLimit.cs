9bb11c48bb57caab249570b0bd7ac6925938769e
Furaneol <km_katsu00@hotmail.com>
2018-11-06 19:04:51 +0900

    TrendRateLimitクラスを定義

--- TwitterLib/RateLimitStatuses/TrendRateLimit.cs
+++ TwitterLib/RateLimitStatuses/TrendRateLimit.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TwitterLib.RateLimitStatuses
 {
-    
-    class TrendRateLimit
+    /// <summary>
+    /// Trendsカテゴリのレート制限情報を格納するオブジェクトです。
+    /// </summary>
+    [DataContract]
+    public class TrendRateLimit
     {
+        /// <summary>
+        /// /trends/closestの取得回数制限を取得します。
+        /// </summary>
+        [DataMember(Name ="/trends/closet")]
+        public RateLimitStatus Closest { get; private set; }
+        /// <summary>
+        /// /trends/availableの取得回数制限を取得します。
+        /// </summary>
+        [DataMember(Name = "/trends/available")]
+        public RateLimitStatus Available { get; private set; }
+        /// <summary>
+        /// /trends/placeの取得回数制限を取得します。
+        /// </summary>
+        [DataMember(Name ="/trends/place")]
+        public RateLimitStatus Place { get; private set; }
     }
 }
