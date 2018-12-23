48cd4dc41af3298953960bb343c3bbfb0fa61b48
Furaneol <km_katsu00@hotmail.com>
2018-12-23 18:05:24 +0900

    TrendContainer型の追加

---
+++ TwitterLib/Trends/TrendContainer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterLib.Trends
+{
+    /// <summary>
+    /// トレンド情報コンテナです。
+    /// </summary>
+    [DataContract]
+    class TrendContainer : ApiResponce
+    {
+        public override Twitter Parent { get; internal set; }
+        [DataMember(Name ="trends")]
+        public TrendInfo[] Trends { get; private set; }
+    }
+}
