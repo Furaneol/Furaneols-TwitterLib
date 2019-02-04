b3f65e297d8b946b719aa03c5855721d7e9d056b
Furaneol <km_katsu00@hotmail.com>
2019-02-04 19:21:32 +0900

    ExtendedEntityクラスを追加

---
+++ TwitterLib/Entities/ExtendedEntities/ExtendedEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace TwitterLib.Entities.ExtendedEntities
+{
+    /// <summary>
+    /// 拡張エンティティ情報を取得します。
+    /// </summary>
+    [DataContract]
+    public class ExtendedEntity
+    {
+        /// <summary>
+        /// 格納されているメディア情報を取得します。
+        /// </summary>
+        [DataMember(Name = "media", IsRequired = false)]
+        public ExtendedMedia[] MediaList { get; }
+    }
+}
